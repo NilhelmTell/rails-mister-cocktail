99028e0aff58c5840e0d593cd198609d2d7e9111
Niccolo Manco <nickneuke@gmail.com>
2020-02-20 19:01:47 +0100

    cocktail back end

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,29 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+    @new_cocktail = Cocktail.new
+  end
+
+  def new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    # Only allow a list of trusted parameters through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+end
