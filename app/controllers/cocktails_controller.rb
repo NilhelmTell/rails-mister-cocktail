9604add6638cc09b92d0d44541d1b8e272fccfdc
Niccolo Manco <nickneuke@gmail.com>
2020-02-21 19:16:34 +0100

    second try

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,6 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
     @new_cocktail = Cocktail.new
@@ -17,12 +19,22 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
   end
 
+  def edit
+
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktails_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
     redirect_to cocktails_path
   end
@@ -30,7 +42,11 @@ class CocktailsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     # Only allow a list of trusted parameters through.
-    def cocktail_params
-      params.require(:cocktail).permit(:name, :photo)
-    end
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
 end
